f129facde54897c3e1d691009f95791e04f224de
abanoubgmousa <abanoubg2022@gmail.com>
2023-08-08 07:44:25 +0300

    change multi bloc provider

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ import 'package:note_app/simple_bloc_observer.dart';
 void main() async {
   Bloc.observer = SimpleBlocObserver();
   await Hive.initFlutter();
-  await Hive.openBox<NoteModel>(kNotesBox);
   Hive.registerAdapter(NotesModelAdapter());
+  await Hive.openBox<NoteModel>(kNotesBox);
   runApp(const Rootapp());
 }
--- lib/root_app/root_app.dart
+++ lib/root_app/root_app.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:note_app/cubits/add_note_cubit/add_note_cubit.dart';
 import 'package:note_app/screens/home_screen_view.dart';
 
 class Rootapp extends StatelessWidget {
@@ -8,15 +6,10 @@ class Rootapp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MultiBlocProvider(
-      providers: [
-        BlocProvider(create: (context) => AddNoteCubit()),
-      ],
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(brightness: Brightness.dark, fontFamily: "Poppins"),
-        home: const HomeScreenView(),
-      ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(brightness: Brightness.dark, fontFamily: "Poppins"),
+      home: const HomeScreenView(),
     );
   }
 }
